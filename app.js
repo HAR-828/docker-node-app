5d0c91fd8beb10d49f5bcebc849aeb98746a01e3
HAR-828 <kharshini2006k@gmail.com>
2026-09-21 21:35:51 +0530

    Change response message to 'Hello from Jenkins CI/CD!'

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from Docker!');
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello from Jenkins CI/CD!');
 });
 
 server.listen(3000, () => {
